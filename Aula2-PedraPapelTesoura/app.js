d8505bc65f14d1ae9078bf0bedc7de88c32617ca
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2021-08-12 09:23:20 -0300

    adicionado aula 2

---
+++ Aula2-PedraPapelTesoura/app.js
@@ -0,0 +1,38 @@
+const pontosJogador = 0;
+const pontosComputador = 0;
+const arr = [["pedra", "tesoura"], ["tesoura", "papel"], ["papel", "pedra"]]; // array de comparação
+const jogadorGanhouPartida = (arrayEscolhasJogadorPc) => arr.some(item => item.toString() == arrayEscolhasJogadorPc.toString())
+const validarEscolhaJogador = (escolhaDigitada) => ["pedra", "papel", "tesoura"].some(item => item == escolhaDigitada.toLowerCase())
+const escolhasToArray = (escolhaJogador, escolhaPc) => new Array(escolhaJogador, escolhaPc)
+const obterEscolhaAleatoria = () => {
+    let valorAleatorio = Math.random(); // retorna um numero entre 0 e 1
+    return valorAleatorio < 0.3 ? "pedra" : valorAleatorio < 0.6 ? "papel" : "tesoura"; //dividindo 1 em 3 partes para definar as opções;
+}
+// Loop do jogo
+while (pontosJogador < 3 && pontosComputador < 3) {
+    let valorEscolhido; //escolha do jogador
+
+    do {
+        valorEscolhido = prompt("Pedra, Papel ou Tesoura!");
+    } while (!validarEscolhaJogador(valorEscolhido));
+
+    let valorPC = obterEscolhaAleatoria()
+
+    // se empatou
+    if (valorEscolhido == valorPC) {
+        alert("empatou");
+        continue;
+    }
+    let jogadasArray = escolhasToArray(valorEscolhido, valorPC)
+    //se ganhou
+    if (jogadorGanhouPartida(jogadasArray)) {
+        pontosJogador++; 
+        alert("Você ganhou! total de pontos:" + pontosJogador)
+    }
+    //se perdeu
+    else {
+        pontosComputador++;
+        alert("pontosComputador Ganhou! total de pontos:" + pontosComputador)
+    }
+}
+pontosJogador >= 3 ? alert("Você ganhou!") : alert(" Você perdeu!")
